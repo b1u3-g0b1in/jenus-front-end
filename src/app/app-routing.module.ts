173b99b91c3375a8e78aeaf81b95ca648745ba02
Tanay Kapoor <itstkaay@hotmail.com>
2024-08-15 22:57:28 +0530

    Update page title and add maintenance route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'maintenance', component: MaintenanceComponent },
+  { path: '**', redirectTo: 'maintenance' }, // Redirect all routes to the maintenance page.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+// src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MaintenanceComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
