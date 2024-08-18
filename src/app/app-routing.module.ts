d4d0ea0c71085cb6c94ac24bc06e30907092414c
Tanay Kapoor <itstkaay@hotmail.com>
2024-08-18 23:07:45 +0530

    Add login functionality and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: 'dev-login', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'maintenance', component: MaintenanceComponent },
-  { path: '**', redirectTo: 'maintenance' },
   { path: '', redirectTo: 'maintenance', pathMatch: 'full' },
+  { path: '**', redirectTo: 'maintenance' },
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 // src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, MaintenanceComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, MaintenanceComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
